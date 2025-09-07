8f8da2bbd7d0f84a69bfbaa8c3f1d3f237d1193a
Elmosius <2272008@maranatha.ac.id>
2025-09-07 23:28:03 +0700

    fix: bug type any

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import App from './App.vue';
-import LenisVue from "lenis/vue";
 
-createApp(App).use(LenisVue).mount('#app')
+createApp(App).mount('#app');
