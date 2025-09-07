ef65ba7913b818d05e1710b4e77f5e761f8d4b1d
Elmosius <2272008@maranatha.ac.id>
2025-09-07 23:23:18 +0700

    fix: delete lenis

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
-<script setup>
-import gsap from "gsap";
-import { ref, watchEffect } from "vue";
-import { VueLenis } from "lenis/vue";
-import { ScrollTrigger, Observer } from "gsap/all";
-
-import Hero from "./components/ui/Hero/index.js";
-import Navbar from "./components/ui/Navbar/index.js";
-
-gsap.registerPlugin(ScrollTrigger, Observer);
-
-const lenisRef = ref();
-
-watchEffect((onInvalidate) => {
-  if (!lenisRef.value?.lenis) return;
-
-  lenisRef.value.lenis.on("scroll", ScrollTrigger.update);
-
-  function update(time) {
-    lenisRef.value.lenis.raf(time * 1000);
-  }
-  gsap.ticker.add(update);
-
-  gsap.ticker.lagSmoothing(0);
-
-  onInvalidate(() => {
-    gsap.ticker.remove(update);
-  });
-});
+<script setup lang="ts">
+import Hero from './components/ui/Hero/index.js';
+import Navbar from './components/ui/Navbar/index.js';
 </script>
 
 <template>
   <div class="h-screen w-screen bg-primary overflow-hidden">
     <Navbar />
-    <VueLenis root ref="lenisRef" :options="{ autoRaf: false }" />
     <Hero />
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
 import LenisVue from "lenis/vue";
 
 createApp(App).use(LenisVue).mount('#app')
