d11cb4c5f7d9ca30e983fb7bb666d65262c4788b
Elmosius <2272008@maranatha.ac.id>
2025-09-07 22:57:41 +0700

    feat: add object position effect paralax

--- src/components/ui/Hero/Hero.vue
+++ src/components/ui/Hero/Hero.vue
@@ -7,21 +7,32 @@ import { images } from '../../../contants';
 const gallery = ref<HTMLElement | null>(null);
 const center = ref<HTMLElement | null>(null);
 const scrollY = useMouseScroll();
-const number = ref(1);
 
 onMounted(() => {
   if (!gallery.value) return;
 
+  let pos = 50;
   const imgs = gsap.utils.toArray(gallery.value.children);
   const maxScroll = gallery.value.getBoundingClientRect().width + window.innerWidth / 2;
 
   if (typeof scrollY.setBounds === 'function') scrollY.setBounds(0, maxScroll);
 
-  const toY = gsap.quickTo(gallery.value, 'x', { duration: 0.5, ease: 'back.out' });
+  const toY = gsap.quickTo(gallery.value, 'x', { duration: 2.5, ease: 'back.out' });
 
   watch(scrollY, (newVal) => {
     const clamped = Math.max(0, Math.min(maxScroll, newVal));
     toY(-clamped);
+    pos = clamped * 0.04;
+
+    imgs.forEach((img) => {
+      const image = img as HTMLImageElement;
+
+      const objPos = gsap.quickTo(image, 'objectPosition', {
+        duration: 2.5,
+        ease: 'back.out',
+      });
+      objPos(pos);
+    });
   });
 });
 
@@ -32,19 +43,14 @@ onUnmounted(() => {
 
 <template>
   <section id="hero" class="relative h-screen overflow-hidden">
-    <div class="absolute inset-0 flex justify-center items-center z-10">
-      <p class="p-0.5 bg-secondary rounded-full" ref="center"></p>
-    </div>
     <div ref="gallery" class="absolute left-[80%] gap-5 flex justify-center items-center h-screen w-screen select-none">
       <img v-for="i in images" :key="i" :src="`${i}`" :alt="`${i}`" draggable="false" />
     </div>
   </section>
 
   <section class="relative">
-    <div class="absolute container mx-auto inset-0 flex items-end">
-      <!-- <p class="text-white text-xl pb-25 ms-10 number">{{ number }}</p> -->
-      <!-- <span class="text-white text-xl pb-25">&nbsp;\ 8</span> -->
-      <span class="text-white text-xl pb-25">{{ scrollY }}</span>
+    <div class="absolute container mx-auto inset-0 flex items-end px-10 p-4">
+      <span class="text-white text-xl pb-25 font-semibold">{{ Math.floor(scrollY) }}</span>
     </div>
   </section>
 </template>
